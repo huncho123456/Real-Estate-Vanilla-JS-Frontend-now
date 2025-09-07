5c966a7d77fbe58c3e9209d4d7e2968589b1f9d9
michealsmacbook <micheal.okafor.79677@gmail.com>
2025-09-07 19:35:19 +0100

    deploy to netlify

--- assets/js/login.js
+++ assets/js/login.js
@@ -58,7 +58,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // ⏳ Delay redirect
         setTimeout(() => {
+<<<<<<< HEAD
           window.location.href = "index.html";
+=======
+          window.location.href = "../index.html";
+>>>>>>> cc6e20c (deploy to netlify)
         }, 2000);
 
       } catch (error) {
